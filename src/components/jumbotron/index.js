a2a8d8998a977eae709ff229cb2f4b74883d50f5
Rinon TENDRINOMENA <rinon.ten@onja.org>
2021-01-19 11:58:08 +0300

    Working on the  landing page and its styles

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Item, Inner, Container, Title, SubTitle, Image, Pane } from './styles/jumbotron';
 
-export default function Jumbostron({children, direction = 'row', ...restProps}) {
+export default function Jumbotron({children, direction='row', ...restProps}) {
     return(
         <Item {...restProps}>
-            <Inner direction={direction}></Inner>
+            <Inner direction={direction}>{children}</Inner>
         </Item>
     ) 
 }
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
+    return <Pane {...restProps} > {children} </Pane>
+ }
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+   return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+ }
+
+ Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+ }
+ Jumbotron.Image = function JumbotronImage({children, ...restProps}) {
+    return <Image {...restProps} />
+ }
